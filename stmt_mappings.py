89ab4c86f8559d887657d0ea7289e7cadc1f45f7
GeraldLeeDoesThings <cheeseyboi@enginity.com>
2021-02-13 01:29:26 -0500

    kwargs scoping, function return types

--- stmt_mappings.py
+++ stmt_mappings.py
@@ -1,4 +1,5 @@
 import ast
+import copy
 import compiler
 import warnings
 
@@ -12,8 +13,13 @@ def translate_function_def(astree: ast.FunctionDef, kwargs: dict) -> str:
         argtype = compiler.typemappings[arg.annotation.id]
         header = header + addcomma + argname + ': ' + argtype
         addcomma = ', '
-    header = header + ') {\n'
-    return header.join(['    ' + compiler.translate_stmt(x, kwargs) for x in astree.body]) + '\n}\n'
+    header = header + ') -> ' + compiler.typemappings[astree.type_comment] + ' {\n'
+    body = ''
+    inner_kwargs = copy.deepcopy(kwargs)
+    for node in astree.body:
+        source, inner_kwargs = compiler.translate_stmt(node, inner_kwargs)
+        body = body + source
+    return header + body + '\n}\n'
 
 
 def translate_async_function_def(astree: ast.AsyncFunctionDef, kwargs: dict) -> str:
@@ -28,8 +34,12 @@ def translate_class_def(astree: ast.ClassDef, kwargs: dict) -> str:
     name = astree.identifier
     # TODO: Keywords, decorators, inheritance ("bases")
     classtext = 'struct ' + name + ';\nimpl ' + name + ' {\n'
-    classtext = classtext.join(['   ' + compiler.translate_stmt(x, kwargs) for x in astree.body])
-    return classtext + '\n}\n'
+    classbody = ''
+    inner_kwargs = copy.deepcopy(kwargs)
+    for node in astree.body:
+        source, inner_kwargs = compiler.translate_stmt(node, inner_kwargs)
+        classbody = classbody + source
+    return classtext + classbody + '\n}\n'
 
 
 def translate_return(astree: ast.Return, kwargs: dict) -> str:
@@ -41,13 +51,14 @@ def translate_delete(astree: ast.Delete, kwargs: dict) -> str:
     return ''
 
 
-def translate_assign(astree: ast.Assign, kwargs: dict) -> (str, dict):
+def translate_assign(astree: ast.Assign, kwargs: dict) -> str:
+    # No deepcopy since we want to remember variable assignment
     var_table = kwargs.get('var_table', default=set())
     targets = []
     value = compiler.translate_expr(astree.value, kwargs)
     result = ''
     for target in astree.targets:
-        name = compiler.translate_expr(target, kwargs)
+        name, _ = compiler.translate_expr(target, kwargs)
         targets.append(name)
         if name in var_table:
             result = result + name + ' = ' + value + ';\n'
@@ -56,4 +67,4 @@ def translate_assign(astree: ast.Assign, kwargs: dict) -> (str, dict):
             result = result + 'let mut ' + name + ' = ' + value + ';\n'
             var_table.add(name)
             kwargs['var_table'] = var_table
-    return result, kwargs
+    return result
